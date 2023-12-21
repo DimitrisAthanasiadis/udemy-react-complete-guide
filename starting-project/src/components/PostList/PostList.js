e38e2fad90bc2311a90cc2fbba607a2d6a3fe931
Dimitris Athanasiadis <dimitris.athanasiadis@saphetor.com>
2023-12-21 18:15:13 +0200

    dynamic post content. passed state and state handling in PostList component.

--- starting-project/src/components/PostList/PostList.js
+++ starting-project/src/components/PostList/PostList.js
@@ -1,17 +1,40 @@
 import NewPost from "../NewPost/NewPost";
 import Post from "../Post/Post";
 import classes from "./PostList.module.css";
+import { useState } from "react";
 
 function PostList() {
+    /**
+     * inserted states and change handlers here because
+     * the state needs to be handled as high level as possible (?)
+     * so that it can be passed to lower level components that might
+     * need it.
+     */
+
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     // i can include an empty html tag. acceptable
     // for React because i need to return only ONE component.
     <>
-        <NewPost />
-        <ul className={classes.posts}>
-            <Post author="Jim" body="Jim's text" />
-            <Post author="Dimitris" body="Dimitris' text" />
-        </ul>
+      <NewPost
+        onBodyChange={changeBodyHandler}
+        onAuthorChange={changeAuthorHandler}
+        enteredBody={enteredBody}
+        enteredAuthor={enteredAuthor}
+      />
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+      </ul>
     </>
   );
 }
