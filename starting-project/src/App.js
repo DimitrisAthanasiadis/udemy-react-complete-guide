7ee6c2a761f89ae24525b2d045219a77a6b144e7
Dimitris Athanasiadis <dimitris.athanasiadis@saphetor.com>
2023-12-21 12:58:29 +0200

    dynamic content in Post component by using props

--- starting-project/src/App.js
+++ starting-project/src/App.js
@@ -4,10 +4,8 @@ import Post from './components/Post/Post';
 function App() {
   return (
     <main>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post author="Jim" body="Jim's text" />
+      <Post author="Dimitris" body="Dimitris' text" />
     </main>
   );
 }
--- starting-project/src/components/Post/Post.js
+++ starting-project/src/components/Post/Post.js
@@ -1,12 +1,8 @@
-const names = ["Jim", "Dimitris"];
-
-function Post() {
-  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-
+function Post(props) {
   return (
     <div>
-      <p>{chosenName}</p>
-      <p>Post text</p>
+      <p>{props.author}</p>
+      <p>{props.body}</p>
     </div>
   );
 }
