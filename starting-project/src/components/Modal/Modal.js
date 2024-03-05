64253fecb403ce84ac989bc01c15f868e1b62363
Dimitris Athanasiadis <dimitris.athanasiadis@saphetor.com>
2024-03-05 14:31:49 +0200

    stop closing modal when clicking inside modal

--- starting-project/src/components/Modal/Modal.js
+++ starting-project/src/components/Modal/Modal.js
@@ -6,9 +6,13 @@ function Modal({ children, onClose }) {
     // children is used to pass the containing/wrapped elements.
     // in this case it is the NewPost component.
 
+    const stopPropagation = (e) => {
+        e.stopPropagation();
+    };
+
     return <>
         <div className={classes.backdrop} onClick={onClose} >
-            <dialog open className={classes.modal} >
+            <dialog open className={classes.modal} onClick={stopPropagation} >
                 {children}
             </dialog>
         </div>
