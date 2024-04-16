ec7a1393d0ab46a659e9a168dee72d0397af7e38
Dimitris Athanasiadis <104418562+DathanasiadisSaphetor@users.noreply.github.com>
2024-04-16 13:36:52 +0300

    High speed core React walkthrough and starting project (#1)
    * created starting-project app
    
    * first custom component
    
    * dynamic post name via random choice from array
    
    * reusing components-multiple Post component instances
    
    * dynamic content in Post component by using props
    
    * Post component styling added via CSS module
    
    * moved App component into separate folder in components folder
    
    * created PostList component and updated Post component accordingly
    
    * created NewPost component
    
    * updated starting-project's README.md
    
    * using state to store entered text from inside the form
    
    * dynamic post content. passed state and state handling in PostList component.
    
    * prevent empty post rendering when no author or post body is written
    
    * created NewPost Modal component
    
    * conditional Modal component appearance
    
    * installed react-icons library
    
    * added MainHeader component and new post button opening modal logic
    
    * stop closing modal when clicking inside modal
    
    * properly handle posts and postlist states and empty posts
    
    * prevent new post modal from auto opening on reload
    
    * dummy backend folder/project
    
    * ignore posts.json file
    
    * added POST functionality in PostList
    
    * fetch posts from the backend
    
    * added loading state in posts list
    
    * added error status upon response
    
    * installed react router
    
    * tiny css changes
    
    * refactored Route components and nesting
    
    * implemented linking and navigating
    
    * use loader method for posts
    
    * implemented submit form via action function
    
    * minor modal css change
    
    * implemented dynamic routes and post details functionality

---
+++ starting-project/src/components/PostList/PostList.js
@@ -0,0 +1,42 @@
+import { useLoaderData } from "react-router-dom";
+import Post from "../Post/Post";
+import classes from "./PostList.module.css";
+
+function PostList() {
+  /**
+   * inserted states and change handlers here because
+   * the state needs to be handled as high level as possible (?)
+   * so that it can be passed to lower level components that might
+   * need it.
+   */
+
+  const posts = useLoaderData();
+
+  return (
+    // i can include an empty html tag. acceptable
+    // for React because i need to return only ONE component.
+    <>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post
+              id={post.id}
+              key={post.body}
+              author={post.author}
+              body={post.body}
+            />
+          ))}
+        </ul>
+      )}
+
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "black" }}>
+          <h2>There are no posts yet</h2>
+          <p>Start adding some</p>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default PostList;
---
+++ starting-project/src/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Posts, { loader as postsLoader } from "./routes/Posts/Posts";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import NewPost, { action as newPostAction } from "./routes/NewPost/NewPost";
+import RootLayout from "./routes/RootLayout/RootLayout";
+import PostDetails, {
+  loader as postDetailsLoader,
+} from "./routes/PostDetails/PostDetails";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// children property is used in the router elements so that the
+// children can be rendered under their parent. i also need an
+// Outlet element to wrapt the children.
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Posts />,
+        loader: postsLoader,
+        children: [
+          { path: "/create-post", element: <NewPost />, action: newPostAction }, // action is caught by react router and used to handle the submit action
+          {
+            path: "/:id",
+            element: <PostDetails />,
+            loader: postDetailsLoader,
+          },
+        ],
+      },
+    ],
+  },
+]); // takes an array of routes i want to have
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
